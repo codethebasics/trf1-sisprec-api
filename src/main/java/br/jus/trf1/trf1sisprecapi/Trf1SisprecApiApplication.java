86eb4f27c92fb7c863a54042e1c8ff50e8525982
Bruno Carneiro <tr301605@trf1.jus.br>
2023-08-19 13:05:09 -0300

    setup

---
+++ src/main/java/br/jus/trf1/trf1sisprecapi/Trf1SisprecApiApplication.java
@@ -0,0 +1,13 @@
+package br.jus.trf1.trf1sisprecapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trf1SisprecApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trf1SisprecApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/jus/trf1/trf1sisprecapi/Trf1SisprecApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jus.trf1.trf1sisprecapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trf1SisprecApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
