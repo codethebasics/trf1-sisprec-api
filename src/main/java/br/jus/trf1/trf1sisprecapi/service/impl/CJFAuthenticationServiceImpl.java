6ca976beca98a8ccfee78247068f39f6d040c39a
Bruno Carneiro <tr301605@trf1.jus.br>
2023-08-24 16:50:37 -0300

    logging + maven profile

--- src/main/java/br/jus/trf1/trf1sisprecapi/service/impl/CJFAuthenticationServiceImpl.java
+++ src/main/java/br/jus/trf1/trf1sisprecapi/service/impl/CJFAuthenticationServiceImpl.java
@@ -3,7 +3,9 @@ package br.jus.trf1.trf1sisprecapi.service.impl;
 import br.jus.trf1.trf1sisprecapi.model.dto.CJFAuthenticationRequest;
 import br.jus.trf1.trf1sisprecapi.model.dto.cjf.SwaggerUsuarioRetorno;
 import br.jus.trf1.trf1sisprecapi.service.CJFAuthenticationService;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,11 +28,27 @@ public class CJFAuthenticationServiceImpl implements CJFAuthenticationService {
 
     @Override
     public SwaggerUsuarioRetorno cjfAuthentication(CJFAuthenticationRequest cjfAuthenticationRequest) {
-        return this.restTemplate.postForEntity(
+
+        // Realizando a autentação no endpoint do CJF
+        SwaggerUsuarioRetorno swaggerUsuarioRetorno = this.restTemplate.postForEntity(
                         CJF_AUTHENTICATION_ENDPOINT,
                         cjfAuthenticationRequest,
                         SwaggerUsuarioRetorno.class
                 )
                 .getBody();
+
+        // Valida se a token foi retornada
+        assert swaggerUsuarioRetorno != null : "A requisição não retornou o response";
+        assert swaggerUsuarioRetorno.getRetorno() != null : "Erro ao obter o retorno da autenticação";
+        assert Strings.isNotBlank(swaggerUsuarioRetorno.getRetorno().getToken()) : "A token de autenticação não foi retornada";
+
+        // Adicionando a token obtida no header da requisição
+        this.restTemplate.getInterceptors().add((request, body, execution) -> {
+            HttpHeaders headers = request.getHeaders();
+            headers.add("Authorization", "Bearer " + swaggerUsuarioRetorno.getRetorno().getToken());
+            return execution.execute(request, body);
+        });
+
+        return swaggerUsuarioRetorno;
     }
 }
